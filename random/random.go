558a6bd6b9f923123c2dfb060dc90f26cc127842
A. Mioranza <amioranza@mdcnet.ninja>
2018-03-30 21:54:29 -0300

    Many adjustments and optimizations, you can generate a random using only the max value or specify a range using min and max  values.

--- random/random.go
+++ random/random.go
@@ -9,6 +9,10 @@ https://github.com/amioranza/random
 
 Usage:
 
+	random <MAX>
+
+	<MAX> is the highest number to guess
+
 	random <MIN> <MAX>
 
 	<MIN> is the lowest number to guess
@@ -26,24 +30,28 @@ import (
 	"time"
 )
 
+// genRandom generatas the random number based on the nanosec of time
 func genRandom(max uint) uint {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(int(max) + 1)
-	switch {
-	case r == 0:
+	if r == 0 {
 		log.Println("Rand generated number zero, changing to number one")
 		r++
-	default:
+	} else {
 		log.Printf("Rand generated number %v is ok\n", r)
 	}
 	return uint(r)
 }
 
+// genRandom2 fills a slice with the range of the numbers specified as parameters and calls genRandom
+// to guess the index of the slice to get the number guessed
 func genRandom2(beg, end int) int {
 	var numRange []int
+
 	if beg > end {
 		log.Fatal("ERROR: Begin greater than end.")
 	}
+
 	size := (end - beg) + 1
 	log.Printf("Random number generator: Begin=%v and End=%v, Slice size=%v\n", beg, end, size)
 
@@ -52,35 +60,37 @@ func genRandom2(beg, end int) int {
 		numRange = append(numRange, num)
 		//fmt.Printf("idx: %v, num: %v\n", i, num)
 	}
+
 	log.Println("Slice: ", numRange)
+
 	idx := genRandom(uint(size))
+
 	return numRange[idx-1]
 }
 
+func parseArg(param string) int {
+	arg, err := strconv.Atoi(param)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return arg
+}
+
 func main() {
-	// Validates the number of arguments
-	if len(os.Args) <= 1 || len(os.Args) <= 2 || len(os.Args) > 3 {
+	switch {
+	case len(os.Args) == 2:
+		max := uint(parseArg(os.Args[1]))
+		random := genRandom(uint(max))
+		fmt.Printf("The guessed number is >>>>>>>>>> %v <<<<<<<<<<<<\n", random)
+	case len(os.Args) == 3:
+		min := parseArg(os.Args[1])
+		max := parseArg(os.Args[2])
+		random := genRandom2(int(min), int(max))
+		fmt.Println("")
+		fmt.Printf("The guessed number is >>>>>>>>>> %v <<<<<<<<<<<<\n", random)
+	default:
 		fmt.Println("Usage:")
 		fmt.Println("\trandom <MIN> <MAX>")
-		log.Fatal("Please inform the <MIN> and <MAX> values")
+		fmt.Println("\tPlease inform the <MIN> and <MAX> values")
 	}
-	// Parse arguments as unsigned integers
-	min, err1 := strconv.ParseUint(os.Args[1], 0, 64)
-	if err1 != nil {
-		panic(err1)
-	}
-	max, err2 := strconv.ParseUint(os.Args[2], 0, 64)
-	if err2 != nil {
-		panic(err2)
-	}
-
-	// Simple random generator, only work with max
-	//random := genRandom(uint(max))
-	//fmt.Printf("1: The generated number is: %v\n", random)
-
-	// Advanced random generator using a slice and calculating the size of the slice, filling slice
-	// and using the simple rand generator to guess the index of the sliec to return the real number guessed
-	random2 := genRandom2(int(min), int(max))
-	fmt.Println("")
-	fmt.Printf("The guessed number is >>>>>>>>>> %v <<<<<<<<<<<<\n", random2)
 }
